8407ad4e0507af06eff59b8dac2751e89ab9c442
Daboss ishere <deadlycheckmate@gmail.com>
2023-05-13 12:23:43 -0400

    basic navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
-
-const NavBar = () => {
-    return (
-        <div>
-           navbar
-        </div>
-    );
-};
-
-export default NavBar;
+export default function NavBar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">Site Name</a>
+        <ul>
+           <li className="active">
+                <a href = "/pricing">Pricing</a>
+            </li> 
+            <li>
+                <a href = "/about">About</a>
+            </li> 
+        </ul>
+    </nav>
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "./App.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
